b6ecf544f00c8c82cf1b67c6788cecb29cf9134b
Mexahoid <mirramax97@Mail.ru>
2021-10-27 12:01:28 +0300

    Modified Query.slq
    Now it creates tables and inserts test data if not exists
    Related to #2

--- DbLibrary/scripts/Query.sql
+++ DbLibrary/scripts/Query.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE [dbo].[Persons]
+﻿IF NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[Persons]'))
+BEGIN
+CREATE TABLE [dbo].[Persons]
 (
 	[ID] INT PRIMARY KEY IDENTITY,
 	[Login] VARCHAR(40) NOT NULL UNIQUE,
@@ -6,6 +9,19 @@
 	[Role] SMALLINT NOT NULL,
 );
 
+INSERT INTO [dbo].[Persons] ([Login], [Password], [Role]) VALUES ('Admin', 'Admin', 1);
+INSERT INTO [dbo].[Persons] ([Login], [Password], [Role]) VALUES ('Test1', 'Pass1', 0);
+INSERT INTO [dbo].[Persons] ([Login], [Password], [Role]) VALUES ('Test2', 'Pass2', 0);
+INSERT INTO [dbo].[Persons] ([Login], [Password], [Role]) VALUES ('Test3', 'Pass1', 0);
+
+END
+
+
+
+
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[Persons]'))
+BEGIN
 CREATE TABLE [dbo].[CheckRequest]
 (
 	[ID] INT PRIMARY KEY IDENTITY,
@@ -16,6 +32,17 @@ CREATE TABLE [dbo].[CheckRequest]
 	CONSTRAINT FK_CheckRequest_To_Persons FOREIGN KEY (PersonID) REFERENCES [dbo].[Persons] (ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+INSERT INTO [dbo].[CheckRequest] ([HostAddress], [Cron], [PersonID]) VALUES ('google.com', '1 * * * *', (SELECT ID FROM [dbo].[Persons] WHERE [Login] = 'Test1'));
+INSERT INTO [dbo].[CheckRequest] ([HostAddress], [Cron], [PersonID]) VALUES ('vsu.ru', '3 * * * *', (SELECT ID FROM [dbo].[Persons] WHERE [Login] = 'Test1'));
+INSERT INTO [dbo].[CheckRequest] ([HostAddress], [Cron], [PersonID]) VALUES ('yandex.ru', '5 * * * *', (SELECT ID FROM [dbo].[Persons] WHERE [Login] = 'Test2'));
+INSERT INTO [dbo].[CheckRequest] ([HostAddress], [Cron], [PersonID]) VALUES ('ibm.com', '1 * * * *', (SELECT ID FROM [dbo].[Persons] WHERE [Login] = 'Test3'));
+INSERT INTO [dbo].[CheckRequest] ([HostAddress], [Cron], [PersonID]) VALUES ('youtube.com', '1 * * * *', (SELECT ID FROM [dbo].[Persons] WHERE [Login] = 'Test3'));
+
+END
+
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[Persons]'))
+BEGIN
 CREATE TABLE [dbo].[CheckHistory]
 (
 	[ID] INT PRIMARY KEY IDENTITY,
@@ -25,3 +52,5 @@ CREATE TABLE [dbo].[CheckHistory]
 
 	CONSTRAINT FK_CheckHistory_To_CheckRequest FOREIGN KEY (CheckID) REFERENCES [dbo].[CheckRequest] (ID) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
+END
+
